f6dcf756cabad78ccf8a394ec7f448c3ef3b637d
Karolina Smolińska <U560477@ent.off.lsy.pl>
2022-09-16 08:24:58 +0200

    15.4

---
+++ first-react-app/src/App.js
@@ -0,0 +1,16 @@
+import Hero from './components/Hero/Hero';
+import SearchForm from './components/SearchForm/SearchForm';
+import List from './components/List/List.js';
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
