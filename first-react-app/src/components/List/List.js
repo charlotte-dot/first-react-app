664138d3016866a332bb8258d296ee6ecbb53180
Karolina Smolińska <U560477@ent.off.lsy.pl>
2022-09-19 20:55:49 +0200

    15.6

--- first-react-app/src/components/List/List.js
+++ first-react-app/src/components/List/List.js
@@ -1,23 +1,30 @@
-import styles from './List.module.scss'
+import styles from './List.module.scss';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
+import { useState } from 'react';
+import shortid from 'shortid';
 
-const List = props => {
+const List = () => {
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon }]);
+    };
+
     return (
-        <div>
+        <div className={styles.list}>
             <header className={styles.header}>
-                <h2>Things to do<span>soon!</span></h2>
+                <h2 className={styles.title}>Things to do<span>soon!</span></h2>
             </header>
-            <p>Interesting things I want to check out</p>
+            <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <article>
-                    <h2 className={styles.title}>< span className={styles.icon + ' fa fa-book' + props.icon} />Books</h2>
-                </article>
-                <article>
-                    <h2 className={styles.title}>< span className={styles.icon + ' fa fa-film' + props.icon} />Movies</h2>
-                </article>
-                <article>
-                    <h2 className={styles.title}>< span className={styles.icon + ' fa fa-gamepad' + props.icon} />Games</h2>
-                </article>
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            <ColumnForm action={addColumn} />
         </div>
     );
 };
