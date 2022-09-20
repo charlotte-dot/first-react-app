55159f8d36ca8b4bbd77e4a3503526ac57be964a
charlotte-dot <k.smolinska90@gmail.com>
2022-09-20 22:46:39 +0200

    17.2

--- first-react-app/src/components/Column/Column.js
+++ first-react-app/src/components/Column/Column.js
@@ -1,8 +1,11 @@
 import styles from './Column.module.scss'
 import Card from './../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from "react-redux";
 
 const Column = props => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-icon' + props.icon} />{props.title}</h2>
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#root')
 );
