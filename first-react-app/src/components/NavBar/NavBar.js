a3e9346200e653c4d044cc1c0a1daaa20a7127ae
charlotte-dot <k.smolinska90@gmail.com>
2022-09-22 21:15:18 +0200

    18.2

--- first-react-app/src/components/NavBar/NavBar.js
+++ first-react-app/src/components/NavBar/NavBar.js
@@ -1,10 +1,19 @@
-const NavBar = () => {
-    return (
-      <nav>
-        <Container/>
-      </nav>
+import styles from '../NavBar/NavBar.module.scss';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-    )
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        <ul>
+        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to='/'>Home</NavLink></li>
+          <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}to='/favorite'>Favorite</NavLink></li>
+          <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}to='/about'>About</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
